e7a83c262359ff6d7b2ce929718cb384b53b3c7b
Mariusz <mariusz9dabal@gmail.com>
2023-11-06 18:36:21 +0100

    fix

--- src/index.js
+++ src/index.js
@@ -1,36 +1,33 @@
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
+import "@fontsource/roboto/300.css"
+import "@fontsource/roboto/400.css"
+import "@fontsource/roboto/500.css"
+import "@fontsource/roboto/700.css"
 
-import * as React from "react";
-import * as ReactDOM from "react-dom/client";
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-import "./index.css";
-import Root from "./routes/root";
-import SetTime from "./routes/setTime";
-import ErrorPage from "./error-page";
-import App from './App';
+import * as React from "react"
+import * as ReactDOM from "react-dom/client"
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import "./index.css"
+import Root from "./routes/root"
+import SetTime from "./routes/setTime"
+import ErrorPage from "./error-page"
+import App from "./App"
 
 const router = createBrowserRouter([
   {
-    path: "/",
+    path: "/CountDown",
     element: <Root />,
-    errorElement: <ErrorPage />
+    errorElement: <ErrorPage />,
   },
   {
     path: "/settime",
-    element: <SetTime />
+    element: <SetTime />,
   },
-]);
+])
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App >
+    <App>
       <RouterProvider router={router} />
     </App>
   </React.StrictMode>
-);
+)
